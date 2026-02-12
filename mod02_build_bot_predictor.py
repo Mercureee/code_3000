6b995194dad5efe33152df20dfe45d8428c932bf
Jayden Chu <jayden.chu@uconn.edu>
2026-02-12 09:37:13 -0500

    Adjusted the values of estimators and max_depth to get a lower misclassification rate

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -69,8 +69,8 @@ def train_model(X, y, seed=seed):
     """
     model = GradientBoostingClassifier(
         learning_rate=0.1,
-        n_estimators=300,
-        max_depth=20,
+        n_estimators=200,
+        max_depth=1,
         subsample=1,
         min_samples_leaf=3,
         random_state=seed
